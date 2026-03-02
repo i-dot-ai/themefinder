84d170b19b11ad52ed69955ddaa0420042e2eee4
Michael Pryse-Davies <michael.pryse-davies@cabinetoffice.gov.uk>
2026-03-02 22:49:40 +0000

    Linter fix

--- tests/test_llm_batch_processor.py
+++ tests/test_llm_batch_processor.py
@@ -70,7 +70,9 @@ async def test_retries(mock_llm, sample_df):
                 [],
             ),  # Second call succeeds
         ]
-        result, _ = await detail_detection(sample_df, mock_llm, question="doesn't matter")
+        result, _ = await detail_detection(
+            sample_df, mock_llm, question="doesn't matter"
+        )
         assert isinstance(result, pd.DataFrame)
         assert mock_call_llm.call_count == 2
 
