865983d605c573c52608d7e994ee548211b7d3c1
gecBurton <g.e.c.burton@gmail.com>
2026-03-04 15:14:10 +0000

    added theme-set rules

--- src/themefinder/__init__.py
+++ src/themefinder/__init__.py
@@ -9,6 +9,12 @@ from .tasks import (
     detail_detection,
 )
 
+from themeset_rules import (
+    rule_1_total_theme_number_less_than_70,
+    rule_2_themes_must_have_a_non_negligible_number_of_responses,
+    rule_3_semantic_similarity_must_be_less_than_90pc,
+    rule_4_themes_should_not_overlap,
+)
 __all__ = [
     "LLM",
     "LLMResponse",
@@ -20,5 +26,10 @@ __all__ = [
     "theme_mapping",
     "theme_refinement",
     "detail_detection",
+    "rule_1_total_theme_number_less_than_70",
+    "rule_2_themes_must_have_a_non_negligible_number_of_responses",
+    "rule_3_semantic_similarity_must_be_less_than_90pc",
+    "rule_4_themes_should_not_overlap",
+
 ]
 __version__ = "0.8.0"
